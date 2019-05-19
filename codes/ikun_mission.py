9588a3aa8bbc82d161c8008b09e51d7d2905333b
Myles Liu <root@IcySakura-SB2.localdomain>
2019-05-19 13:16:15 -0700

    init with codes

---
+++ codes/ikun_mission.py
@@ -0,0 +1,51 @@
+try:
+    from malmo import MalmoPython
+except:
+    import MalmoPython
+
+import os
+import sys
+import time
+import json
+
+def main():
+    # Start mission
+    # Create default Malmo objects:
+    agent_host = MalmoPython.AgentHost()
+    try:
+        agent_host.parse( sys.argv )
+    except RuntimeError as e:
+        print('ERROR:',e)
+        print(agent_host.getUsage())
+        exit(1)
+    if agent_host.receivedArgument("help"):
+        print(agent_host.getUsage())
+        exit(0)
+
+    if agent_host.receivedArgument("test"):
+        num_repeats = 1
+    else:
+        num_repeats = 10
+
+    for i in range(num_repeats):
+        size = int(6 + 0.5*i)
+        print("Size of maze:", size)
+        my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i/20.0), size), True)
+        my_mission_record = MalmoPython.MissionRecordSpec()
+        my_mission.requestVideo(800, 500)
+        my_mission.setViewpoint(1)
+        # Attempt to start a mission:
+        max_retries = 3
+        my_clients = MalmoPython.ClientPool()
+        my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
+
+        for retry in range(max_retries):
+            try:
+                agent_host.startMission( my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i) )
+                break
+            except RuntimeError as e:
+                if retry == max_retries - 1:
+                    print("Error starting mission", (i+1), ":",e)
+                    exit(1)
+                else:
+                    time.sleep(2)
