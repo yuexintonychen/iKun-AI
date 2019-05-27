a13ab357ec047cc47de2a1f18a459a6d3fca91f2
yuexinc1 <yuexinc1@uci.edu>
2019-05-26 21:46:09 -0700

    Main Build Complete

--- codes/ikun_mission.py
+++ codes/ikun_mission.py
@@ -17,7 +17,13 @@ import numpy as np
 AGENT_OBSERVATION_LENGTH = 61
 
 # size of the maze
-size_of_maze = 7
+size_of_maze = 21
+
+# original matrix in 2D
+matrix2dOriginal = []
+
+# maze_map
+maze_map =[]
 
 # 0 means front; 1 means right; 2 means back; 3 means left (in default direction: 0)
 agent_current_direction = 0
@@ -29,6 +35,9 @@ agent_current_position_index_in_grid = -1
 # Record the maze map position in the actual map
 first_block_index_in_actual_map = -1
 last_block_index_in_actual_map = -1
+
+#malmo agent
+agent_host = None
 
 # Helpful Classes or functions not related to the core of the project
 
@@ -296,7 +305,7 @@ def is_reachable(block):
 
 def is_in_map(block):
     # Return true is the block is inside the map; otherwise, return false.
-    return block in ["diamond_block", "redstone_block", "emerald_block", "flowing_lava"]
+    return block in ["diamond_block", "redstone_block", "emerald_block", "flowing_lava", "lava"]
 
 def get_maze_map(original_grid):
     # get the actual map of maze in the word
@@ -310,6 +319,7 @@ def get_maze_map(original_grid):
             if first_block_index_in_actual_map == -1:
                 first_block_index_in_actual_map = index
             last_block_index_in_actual_map = index
+            #print("Next one after:", block, "is", original_grid[index+1])
     return temp_grid
 
 def find_start_end(grid):
@@ -349,9 +359,9 @@ def agent_turn_right():
 def move_forward(agent_host):
     # move 1 to the agent's current direction(fake)
     # Also deal with the agent position variables (both)
-
-    global agent_current_position_xy_in_maze, agent_current_position_index_in_grid
-
+    global agent_current_position_xy_in_maze, agent_current_position_index_in_grid, matrix2dOriginal, maze_map
+    backup_xy_in_maze = copy.deepcopy(agent_current_position_xy_in_maze)
+    """
     if agent_current_direction == 0:
         agent_host.sendCommand("movesouth 1")
         agent_current_position_xy_in_maze[1] += 1
@@ -368,6 +378,57 @@ def move_forward(agent_host):
         agent_host.sendCommand("moveeast 1")
         agent_current_position_xy_in_maze[0] += 1
         agent_current_position_index_in_grid += 1
+    """
+    print("agent_current_direction: ",agent_current_direction)
+
+    #JUST FOR TEST
+    if agent_current_direction == 0:
+        
+        agent_current_position_xy_in_maze[1] += 1
+        agent_current_position_index_in_grid += AGENT_OBSERVATION_LENGTH
+        x,y = agent_current_position_xy_in_maze[0], agent_current_position_xy_in_maze[1]
+        #We use formula caclulating maze map index to check the next step
+        print(len(maze_map))
+        print(maze_map[size_of_maze*(y+1)-(size_of_maze-x)])
+        if (maze_map[size_of_maze*(y+1)-(size_of_maze-x)] == "diamond_block"):
+            agent_host.sendCommand("movesouth 1")
+        else:
+            print('it cannot move')
+            agent_current_position_xy_in_maze = backup_xy_in_maze
+            
+    elif agent_current_direction == 1:
+       
+        agent_current_position_xy_in_maze[0] -= 1
+        agent_current_position_index_in_grid -= 1
+        x,y = agent_current_position_xy_in_maze[0], agent_current_position_xy_in_maze[1]
+        if(maze_map[size_of_maze*(y+1)-(size_of_maze-x)] == "diamond_block"):
+            agent_host.sendCommand("movewest 1")
+        else:
+            print('it cannot move')
+            agent_current_position_xy_in_maze = backup_xy_in_maze
+        
+    elif agent_current_direction == 2:
+        
+        agent_current_position_xy_in_maze[1] -= 1
+        agent_current_position_index_in_grid -= AGENT_OBSERVATION_LENGTH
+        x,y = agent_current_position_xy_in_maze[0], agent_current_position_xy_in_maze[1]
+        if(maze_map[size_of_maze*(y+1)-(size_of_maze-x)] == "diamond_block"):
+            agent_host.sendCommand("movenorth 1")
+        else:
+            print('it cannot move')
+            agent_current_position_xy_in_maze = backup_xy_in_maze
+        
+    elif agent_current_direction == 3:
+        
+        agent_current_position_xy_in_maze[0] += 1
+        agent_current_position_index_in_grid += 1
+        x,y = agent_current_position_xy_in_maze[0], agent_current_position_xy_in_maze[1]
+        if(maze_map[size_of_maze*(y+1)-(size_of_maze-x)] == "diamond_block"):
+            agent_host.sendCommand("moveeast 1")
+        else:
+            print('it cannot move')
+            agent_current_position_xy_in_maze = backup_xy_in_maze
+        
 
 def get_xy_position_of_maze_map_by_position_of_actual_map(\
     position_in_actual_map, grid):
@@ -395,22 +456,29 @@ def test_moving(agent_host, directions):
     # e.g. giving directions as [0, 1, 2, 0]
     # direction here is absolute to the agent's current direction
     # p.s. this function will restore the agent's direction back to the original one when it finishes moving
-    print("Before moving, the agent is at xy_maze:", agent_current_position_xy_in_maze, \
-        "and it is at index_of_grid:", agent_current_position_index_in_grid)
+    print("Before moving, the agent is at xy_maze:", agent_current_position_xy_in_maze)
+        #"and it is at index_of_grid:", agent_current_position_index_in_grid)
     global agent_current_direction
     original_direction = agent_current_direction
     for direction in directions:
+        #print('The agent curent direction: ', agent_current_direction)
+        print("The direction: ", direction)
         if direction == 1:
+            print('The agent turn right')
             agent_turn_right()
         elif direction == 2:
             agent_turn_left()
             agent_turn_left()
+            print('The agnet truns back')
         elif direction == 3:
             agent_turn_left()
-        move_forward(agent_host)
+            print('The agent turn left')
+        elif direction == 0:    
+            move_forward(agent_host)
+            print('The agent move forward')
         print("After making a move of [" + str(direction) + "], the agent is at xy_maze:", \
-            agent_current_position_xy_in_maze, "and it is at index_of_grid:", agent_current_position_index_in_grid)
-        agent_current_direction = original_direction
+            agent_current_position_xy_in_maze) #, "and it is at index_of_grid:", agent_current_position_index_in_grid)
+        #agent_current_direction = original_direction
         time.sleep(2)
 
 def execute_action_list(agent_host, directions):
@@ -505,7 +573,25 @@ def get_num_of_walkable_blocks_in_front_of_agent(agent_current_position_xy_in_ma
 
 def maze_to_2dMatrix(maze_map, size_of_maze):
     matrix2d = []
-    print('length of maze_map', len(maze_map))
+    i = 0
+    #print("This is maze_map: ",len(maze_map))
+    while i < len(maze_map):
+        subList = []
+        innerI = 0
+        while innerI < size_of_maze:
+            #print('There is the BUG: ',maze_map[i+innerI])
+            if is_reachable(maze_map[i+innerI]):
+                subList.append(0)
+            else:
+                subList.append(1)
+            innerI += 1
+        matrix2d.append(subList)
+        i += size_of_maze
+    return matrix2d
+
+def maze_to_2dMatrix_reversed(maze_map, size_of_maze):
+    matrix2d = []
+    #print("size of maze:", size_of_maze)
     """for i in range(0, len(maze_map), size_of_maze):
         subList = []
         for innerI in range(0, size_of_maze):
@@ -518,20 +604,36 @@ def maze_to_2dMatrix(maze_map, size_of_maze):
         matrix2d.append(subList)
     """
     i = 0
+    #print("This is maze_map: ",len(maze_map))
     while i < len(maze_map):
         subList = []
         innerI = 0
         while innerI < size_of_maze:
+            #print('There is the BUG: ',maze_map[i+innerI])
             if is_reachable(maze_map[i+innerI]):
-                subList.append(1)
-            else:
                 subList.append(0)
+            else:
+                subList.append(1)
             innerI += 1
         matrix2d.append(subList)
         i += size_of_maze
+
+    reversedMatrix = []
     
-    matrix2d = np.array(matrix2d)
-    return matrix2d
+    i = len(matrix2d) - 1
+    while i >= 0:
+        #print('the i is : ', i)
+        subList = []
+        innerI = len(matrix2d[i])-1
+        while innerI >= 0:
+            subList.append(matrix2d[i][innerI])
+            innerI -= 1
+        reversedMatrix.append(subList)
+        print('THE SUBLIST: ', subList)
+        i -= 1
+
+    matrix2d = np.array(reversedMatrix)
+    return matrix2d.astype(np.float64)
 
 def is_finish(block):
     # return true if the agent is standing in the goal block
@@ -556,16 +658,32 @@ def positionTransition(grid, matrixArray, orientation, size_of_maze):
     newStartPosition = [size_of_maze - startPosition[1]-1, \
         size_of_maze - startPosition[0]-1, newOrientation]
     newStartPosition = np.array(newStartPosition)
-
+    print("The matrix: ",matrixArray)
     concatMatrix = np.concatenate((matrixArray, newStartPosition), axis = None)
     concatMatrix = np.reshape(concatMatrix,(1,-1))
-    print(concatMatrix.shape)
 
-    np.save("hello", concatMatrix)
+    print(concatMatrix)
+
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '../Neural-Localization/test_data/hello.npy')
+    print(file_path)
+    np.save(file_path, concatMatrix)
+
+def directionConvert(direction):
+    converted = direction
+    if direction == 1:
+        converted = 1
+    elif direction == 2:
+        converted = 0
+    elif direction == 0:
+        converted = 3
+    return converted
 
 def main():
     # Start mission
     # Create default Malmo objects:
+    global agent_host
+    global matrix2dOriginal
+    global maze_map
     agent_host = MalmoPython.AgentHost()
     try:
         agent_host.parse( sys.argv )
@@ -623,9 +741,12 @@ def main():
         print("Size of actual map:", len(grid))
         
         maze_map = get_maze_map(grid)
-        print("maze map:", maze_map)
+        print("maze map:", len(maze_map))
+        #print(maze_map[244])
         #The maze construction
-        matrix2d = maze_to_2dMatrix(maze_map, size_of_maze).flatten()
+        matrix2dOriginal = maze_to_2dMatrix(maze_map, size_of_maze)
+        matrix2d = maze_to_2dMatrix_reversed(maze_map, size_of_maze)
+        print("the matrix 2d: ", matrix2d)
         matrixArray = matrix2d.flatten()
 
         start_and_end_positions_in_actual_map = find_start_end(grid)
@@ -664,5 +785,64 @@ def main():
 
         positionTransition(grid, matrixArray, yaw_of_agent, size_of_maze)
 
+        os.system("python ../Neural-Localization/a3c_main.py --num-processes 0 --evaluate 1 --map-size 21 --max-episode-length 60 --load ../Neural-Localization/pretrained_models/m21_l60 --test-data ../Neural-Localization/test_data/hello.npy")
+
+        stringList = []
+        
+        while True:
+            actionHistFile = open("action_history.txt", "r")
+            stringList = actionHistFile.readlines()
+            if (stringList[len(stringList)-1] == "END"):
+                break
+        
+        actionHistFile.close()
+        
+        #actionList = []
+
+        print(stringList)
+        actionCollection = []
+        positionCollection = []
+        for n in range(0, len(stringList)-1):
+            tmp = stringList[n].split(' ')
+            for m in range(0,len(tmp)-1):
+                L = tmp[m].split(',')
+                actionCollection.append(L[0])
+                positionCollection.append([L[1],L[2]])
+
+        print('The original: ',actionCollection)
+        print(positionCollection)
+        """
+        del stringList[-1]
+        for string in stringList:
+            actionCollection = string.split(' ')
+            del actionCollection[-1]
+            for aindex in range(len(actionCollection)):
+                converted = directionConvert(int(actionCollection[aindex]))
+                actionCollection[aindex] = converted
+            actionList.append(actionCollection)
+        """
+        """
+        for testingset in actionList:
+            #check if it's reachable
+
+            test_moving(agent_host, testingset)
+        """
+        actionList = []
+        
+        for index in range(len(actionCollection)):
+            row,col = positionCollection[index][0], positionCollection[index][1]
+            action = actionCollection[index]
+            print(matrix2d[int(row)][int(col)])
+            if matrix2d[int(row)][int(col)] == 0:
+                convertAction = directionConvert(int(action))
+                actionList.append(convertAction)
+        #print('THIS IS THE ACTION: ',len(actionList), actionList)
+        
+        print('The list:', actionList)
+        #raise('STOP HERE')
+        test_moving(agent_host, actionList)
+    
+
+    
 if __name__ == "__main__":
     main()
